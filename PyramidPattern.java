7ea2f7ed56be656af797fb2a6da460c5fc1cb34f
venkateshpandiri2030p <venkatesh.pandiri2030@gmail.com>
2026-09-02 13:47:18 +0530

    Add files via upload

---
+++ PyramidPattern.java
@@ -0,0 +1,21 @@
+public class PyramidPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
